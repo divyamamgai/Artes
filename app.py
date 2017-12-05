9be22f683d35819649b3712cbf99d4308200d7d2
Divya Mamgai <divyamamgai21@gmail.com>
2017-12-05 17:39:51 +0530

    Minor Code Cleanup

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
-from flask import render_template
+import random
+import string
+
+from flask import (Flask,
+                   session as flask_session,
+                   render_template)
 from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
@@ -10,6 +14,12 @@ db = SQLAlchemy(app)
 from models import *
 
 
+def generate_token():
+    return ''.join(
+        random.choice(string.ascii_uppercase + string.digits) for i in
+        range(32))
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -17,7 +27,9 @@ def index():
 
 @app.route('/login', methods=['GET'])
 def login():
-    return render_template('login.html')
+    login_state = generate_token()
+    flask_session['login_state'] = login_state
+    return render_template('login.html', login_state=login_state)
 
 
 if __name__ == '__main__':
--- setup.sh
+++
@@ -1,3 +0,0 @@
-export SECRET_KEY="super_secret_key"
-export DEBUG=TRUE
-export DATABASE_URL="postgres://postgres:postgres@postgres:5432/postgres"
